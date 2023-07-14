3323a7a669c2092d7242ee0468e1ffc424211785
aarya moktan <aaryamoktan2055@gmail.com>
2023-07-14 03:30:12 -0700

    commit first

--- client/src/conent/Nevbar/Nevbar.js
+++ client/src/conent/Nevbar/Nevbar.js
@@ -3,28 +3,31 @@ import { Link } from 'react-router-dom'
 const Nevbar = () => {
   return (
    <>
-    <nav class="navbar navbar-expand-lg navbar-light ">
-  <Link class="navbar-brand" to="/">Blog</Link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item ">
-        <Link class="nav-link" to="/">Home </Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/features">Features</Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/">Pricing</Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link " href="#">Disabled</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
+   <div className='top'>
+    <div className='left'>
+      <div className='icon'>
+        <div className='facebook'><i class="fa-brands fa-facebook "></i></div>
+        <div className='instagram'><i class="fa-brands fa-instagram"></i></div>
+        <div className='twiter'><i class="fa-brands fa-twitter"></i></div>
+        <div className='youtube'><i class="fa-brands fa-youtube " ></i></div>
+      </div>
+    </div>
+    <div className='center'>
+      <ul>
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>CONTACT</li>
+        <li>WRITE</li>
+        <li>LOGOUT</li>
+      </ul>
+    </div>
+    <div className='right'>
+      <img src="https://scontent.fktm18-1.fna.fbcdn.net/v/t39.30808-6/358640149_1764766103993536_7119475189517747661_n.jpg?_nc_cat=103&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=hU9X0NweBEkAX8I9gHr&_nc_ht=scontent.fktm18-1.fna&oh=00_AfAf_R3x8uYJQi71mww1cnFYnmL6__cB21QmQzQGp3vZDA&oe=64B51376" alt="pp"/>
+      <div className='search'>
+      <i class="fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+   </div>
    </>
   )
 }
