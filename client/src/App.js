82d470f9165dcf3642326be26bc7ea365f6d5bb5
aarya moktan <aaryamoktan2055@gmail.com>
2023-07-13 17:15:27 -0700

    commit first

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> lets do it
+    <br/>
+      Blog
+    </div>
+  );
+}
+
+export default App;
