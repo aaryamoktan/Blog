cb78bccecc33e98d0c424c198e8a04b2bb0aaece
aarya moktan <aaryamoktan2055@gmail.com>
2023-07-15 00:20:19 -0700

    commit first

--- client/src/components/Routes/Path.js
+++ client/src/components/Routes/Path.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from '../../conent/Home/Home'
 import Nevbar from '../../conent/Nevbar/Nevbar'
 import { Route,Routes } from 'react-router-dom'
+import Write from '../../conent/write/Write'
 import Singleposst from '../../conent/singlepost/Singleposst'
 const Path = () => {
   return (
@@ -9,6 +10,7 @@ const Path = () => {
     <Nevbar/>
     <Routes>
     <Route path="/" element={<Home/>}/>
+    <Route path="/write" element={<Write/>}/>
     <Route path="/profile" element={<Singleposst/>}/>
     </Routes>
    
--- client/src/conent/singlepost/Singleposst.js
+++ client/src/conent/singlepost/Singleposst.js
@@ -6,21 +6,29 @@ const Singleposst = () => {
     return (
         <>
             <div className='singleContainer'>
-            <Siderbar/>
+                <Siderbar />
                 <div className='singlepost'>
-                <span className='my'>Content</span>
+                    <span className='my'>Content</span>
                     <div className='singleCard'>
                         <div className='singleImg'>
                             <img src={img} alt="/" />
                         </div>
-                        <div className='singletype'>nature
+                        <div className='singleCont'><div className='singleTitle'>
+                            Auther :<i>Aarya Moktan</i>
                         </div>
-                        <div className='singleTitle'>
-                            Aarya Moktan
+                            <div className='singletype'>nature
+                            </div>
+
+                            <div className='singleTime'>
+                                <div className='edit'>
+                                    <div className='edit1'><i class="fa-solid fa-pen-to-square" style={{ "color": "#015cf9" }}></i></div>
+                                    <div class="delete"><i class="fa-solid fa-trash" style={{ "color": "#ff052b" }}></i></div>
+                                </div>
+                                1hrs ago
+                            </div>
+
                         </div>
-                        <div className='singleTime'>
-                            1hrs ago
-                        </div>
+
                         <div className='singleDesc'>
                             <p>I’m a big fan of short sentences. I love them because people can understand them easily. There's great value in short sentences that are readable, digestible, and punchy.
                                 Writers often get   </p>
@@ -30,14 +38,22 @@ const Singleposst = () => {
                         <div className='singleImg'>
                             <img src={img} alt="/" />
                         </div>
+                        <div className='singleCont'><div className='singleTitle'>
+                          Auther :<i>Aarya Moktan</i>
+                        </div>
                         <div className='singletype'>nature
                         </div>
-                        <div className='singleTitle'>
-                            Aarya Moktan
-                        </div>
+                        
                         <div className='singleTime'>
+                        <div className='edit'>
+                            <div className='edit1'><i class="fa-solid fa-pen-to-square" style={{"color": "#015cf9"}}></i></div>
+                            <div class="delete"><i class="fa-solid fa-trash" style={{"color": "#ff052b"}}></i></div>
+                        </div>
                             1hrs ago
                         </div>
+
+                        </div>
+                       
                         <div className='singleDesc'>
                             <p>I’m a big fan of short sentences. I love them because people can understand them easily. There's great value in short sentences that are readable, digestible, and punchy.
                                 Writers often get   </p>
@@ -47,14 +63,22 @@ const Singleposst = () => {
                         <div className='singleImg'>
                             <img src={img} alt="/" />
                         </div>
+                        <div className='singleCont'><div className='singleTitle'>
+                          Auther :<i>Aarya Moktan</i>
+                        </div>
                         <div className='singletype'>nature
                         </div>
-                        <div className='singleTitle'>
-                            Aarya Moktan
-                        </div>
+                        
                         <div className='singleTime'>
+                        <div className='edit'>
+                            <div className='edit1'><i class="fa-solid fa-pen-to-square" style={{"color": "#015cf9"}}></i></div>
+                            <div class="delete"><i class="fa-solid fa-trash" style={{"color": "#ff052b"}}></i></div>
+                        </div>
                             1hrs ago
                         </div>
+
+                        </div>
+                       
                         <div className='singleDesc'>
                             <p>I’m a big fan of short sentences. I love them because people can understand them easily. There's great value in short sentences that are readable, digestible, and punchy.
                                 Writers often get   </p>
@@ -64,38 +88,29 @@ const Singleposst = () => {
                         <div className='singleImg'>
                             <img src={img} alt="/" />
                         </div>
-                        <div className='singletype'>nature
-                        </div>
-                        <div className='singleTitle'>
-                            Aarya Moktan
-                        </div>
-                        <div className='singleTime'>
-                            1hrs ago
-                        </div>
-                        <div className='singleDesc'>
-                            <p>I’m a big fan of short sentences. I love them because people can understand them easily. There's great value in short sentences that are readable, digestible, and punchy.
-                                Writers often get   </p>
-                        </div>
-                    </div>
-                    <div className='singleCard'>
-                        <div className='singleImg'>
-                            <img src={img} alt="/" />
+                        <div className='singleCont'><div className='singleTitle'>
+                          Auther :<i>Aarya Moktan</i>
                         </div>
                         <div className='singletype'>nature
                         </div>
-                        <div className='singleTitle'>
-                            Aarya Moktan
-                        </div>
+                        
                         <div className='singleTime'>
+                        <div className='edit'>
+                            <div className='edit1'><i class="fa-solid fa-pen-to-square" style={{"color": "#015cf9"}}></i></div>
+                            <div class="delete"><i class="fa-solid fa-trash" style={{"color": "#ff052b"}}></i></div>
+                        </div>
                             1hrs ago
                         </div>
+
+                        </div>
+                       
                         <div className='singleDesc'>
                             <p>I’m a big fan of short sentences. I love them because people can understand them easily. There's great value in short sentences that are readable, digestible, and punchy.
+                            
                                 Writers often get   </p>
                         </div>
                     </div>
                 </div>
-              
             </div>
         </>
     )
