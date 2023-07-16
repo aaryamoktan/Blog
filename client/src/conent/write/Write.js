cd10539ea877214943fd2cb4d95aac91633b1d8a
aarya moktan <aaryamoktan2055@gmail.com>
2023-07-15 18:41:48 -0700

    commit first

--- client/src/conent/write/Write.js
+++ client/src/conent/write/Write.js
@@ -6,9 +6,12 @@ const Write = () => {
       <div className='writeContainer'>
         <div className='writeInner'>
           <div className='writeForm'>
-          <form>
-
-          </form>
+            <form>
+            <input type="file"  />
+              <input className='title' placeholder='Title' style={{ border: "none" }} />
+              <br />
+              <input className='story' placeholder=' Tell your stroy...' style={{ border: "none" }} />
+            </form>
           </div>
         </div>
       </div>
