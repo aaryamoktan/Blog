f6a395363ca92c2f439e8a81cbc61c96a5e65938
aarya moktan <aaryamoktan2055@gmail.com>
2023-07-15 19:07:29 -0700

    commit first

--- client/src/conent/write/Write.js
+++ client/src/conent/write/Write.js
@@ -1,16 +1,21 @@
 import React from 'react'
-
+const img = require("../../images/env.jpg")
 const Write = () => {
+  
   return (
     <>
       <div className='writeContainer'>
         <div className='writeInner'>
+        <div className='writeImg'>
+          <img src={img} alt=""/>
+        </div>
           <div className='writeForm'>
             <form>
             <input type="file"  />
               <input className='title' placeholder='Title' style={{ border: "none" }} />
               <br />
               <input className='story' placeholder=' Tell your stroy...' style={{ border: "none" }} />
+              <button type="submit">Post</button>
             </form>
           </div>
         </div>
