182e3de343275e0310e8557f192a497cf7a8aa26
aarya moktan <aaryamoktan2055@gmail.com>
2023-07-20 18:21:37 -0700

    commit first

--- server/router/auth.js
+++ server/router/auth.js
@@ -10,5 +10,44 @@ router.get("/",(req,res)=>
 router.post("/registration",(Req,res)=>
 {
     const  {username,email,password} = req.body;
+    if(!username || !email || !password)
+    {
+        res.status(400).json(Error ="plz fill the form")
+    }
+    else{
+        const find = User.findOne({email:email})
+        if(find)
+        {
+            res.status(400).json(Error = "email already exit")
+        }
+        else{
+
+        }
+    }
+})
+router.post("login",(req,res)=>
+{
+    const {email,password} = req.body;
+    if( !email || !password)
+    {
+        res.status(400).json(Error ="plz fill the form")
+    }
+    else{
+        const find = User.findOne({email:email})
+        if(find)
+        {
+           if(find.password === password)
+           {
+            console.log("welcome to my world");
+           }
+           else{
+            res.status(400).json(Error = "plz enter correct password")
+           }
+        }
+        else{
+            res.status(400).json(Error="input email in not exit")
+        }
+    }
+
 })
 module.exports = router;
