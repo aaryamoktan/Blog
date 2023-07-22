1ddd3c7f109f755750f96503b2d298411c243f84
aarya moktan <aaryamoktan2055@gmail.com>
2023-07-21 19:13:13 -0700

    commit first

--- client/src/conent/footer/Footer.js
+++ client/src/conent/footer/Footer.js
@@ -3,9 +3,27 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <div className='footerContainer'>
-            <div className='footerinner'>
-                welcome to footer world
+         <div className='footerContainer'>
+            <div className='footercontaint1'>
+            <h1>Information</h1>
+            <p>Find best solution for your company ,protfilio,school,college
+            to develop website</p>
+            </div>
+            <div className=' footercontaint2'>
+            <h1>Contact</h1>
+            +977 9803071043
+            <br/>
+            +977 **********
+            <br/>
+           <p>Email:moktanteamwebsolun@gmail.com</p>
+            </div>
+            <div className='footercontaint3'>
+            <h1>Social-Media</h1>
+            <a href="https://www.facebook.com/MT_WEB_Solution-108537245627454" target="blank"><i class="fa-brands fa-square-facebook" style={{"color":"black", "background":"white"}}></i></a>
+            <br/>
+            <a href="/" target="blank"><i class="fa-brands fa-square-instagram" style={{"color":"black", "background":"white"}}></i></a>
+            <br/>
+            <a href="/" target="blank"><i class="fa-brands fa-square-twitter" style={{"color":"black", "background":"white"}}></i></a>
             </div>
         </div>
     </>
