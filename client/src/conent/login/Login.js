d3db9df731f19f96baa3e598324071dc50eed686
aarya moktan <aaryamoktan2055@gmail.com>
2023-07-22 18:14:33 -0700

    commit first

--- client/src/conent/login/Login.js
+++ client/src/conent/login/Login.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+    const [data,setdata] = useState(
+        {
+            email:'',
+            passowrd:''
+        }
+    )
+    const onchange = (e)=>
+    {
+        setdata({...data,[e.target.name]:e.target.value})
+
+    }
+    const sumbit = (e)=>
+    {
+        e.preventDefault();
+    }
     return (
         <>
         <div className='loginContainer'>
         
             <div className='loginInner'>
-                <form> 
+                <form onClick={sumbit}> 
                     <label>Email-Id </label><br/>
-                    <input type="email" placeholder='Enter Your Email-Id.......................'/><br/>
+                    <input type="email" onchnage={onchange} value={data.email} name="email" placeholder='Enter Your Email-Id.......................'/><br/>
                     <label>Password </label><br/>
-                    <input type="password" placeholder='*******************************'/><br/>
+                    <input type="password" onchnage={onchange} value={data.password} name="password" placeholder='*******************************'/><br/>
                     <button type="submit">Login</button>
                 </form>
             </div>
--- client/src/conent/login/Registration.js
+++ client/src/conent/login/Registration.js
@@ -1,20 +1,36 @@
 import React from 'react'
 import LoginRoutes from '../../components/Routes/LoginRoutes'
-const img=require("../../images/log.jpg")
+
 const Registration = () => {
+  const [data,setdata] = useState(
+    {
+        username:'',
+        email:'',
+        passowrd:''
+    }
+)
+const onchange = (e)=>
+{
+    setdata({...data,[e.target.name]:e.target.value})
+
+}
+const submit = (e)=>
+{
+    e.preventDefault();
+}
   return (
     <>
     <LoginRoutes/>
         <div className='registerContainer'>
        
             <div className='registerInner'>
-                <form>
+                <form onClick={submit}>
                     <label>User-Name: </label><br/>
-                    <input type="text" className='input' placeholder='Enter Your Name ......'/><br/>
+                    <input type="text" className='input' onChange={onchange} value={data.username} placeholder='Enter Your Name ......'/><br/>
                     <label>Email-Id </label><br/>
-                    <input type="email" className='input'  placeholder='Enter Your Email-Id ......'/><br/>
+                    <input type="email" className='input' onChange={onchange} value={data.email}  placeholder='Enter Your Email-Id ......'/><br/>
                     <label>Password </label><br/>
-                    <input type="password" className='input' placeholder="**********"/><br/>
+                    <input type="password" className='input' onChange={onchange} value={data.password} placeholder="**********"/><br/>
                     <button type="submit">Register</button>
                 </form>
             </div>
