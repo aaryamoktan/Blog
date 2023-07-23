6d9f6fb168ccaebb550f01d7896d66bc677841af
aarya moktan <aaryamoktan2055@gmail.com>
2023-07-22 18:03:23 -0700

    commit first

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app =express()
-
-
 app.use(express.json())
-
+app.use(express.urlencoded({extended:false}))
 app.use(require("./router/auth"))
 app.listen(5000,(req,res)=>
 {
